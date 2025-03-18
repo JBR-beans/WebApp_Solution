b957f1da8380f4dc6bba3e68e0b16c35fc9ae54e
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2025-03-18 15:14:49 -0700

    post components, routing

--- WebAppClient/src/app/app.module.ts
+++ WebAppClient/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PostListComponent,
+    PostDetailComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
